07b8f15bccbbfd56b65fa71bf0a71469c2aa521d
faizy <faizy7sheikh@gmail.com>
2020-06-05 16:22:45 +0530

    Updates

---
+++ precache-manifest.d25a3b08f1a0909fcc7604b89b801835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23b955f06b6af01eb631ff909b63b9a",
+    "url": "/react-js-resume/index.html"
+  },
+  {
+    "revision": "0c705dce5edd450d0996",
+    "url": "/react-js-resume/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a4c6e3264ac262138fa2",
+    "url": "/react-js-resume/static/css/main.ddf63994.chunk.css"
+  },
+  {
+    "revision": "0c705dce5edd450d0996",
+    "url": "/react-js-resume/static/js/2.6b7d77ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-resume/static/js/2.6b7d77ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c6e3264ac262138fa2",
+    "url": "/react-js-resume/static/js/main.9170a45d.chunk.js"
+  },
+  {
+    "revision": "d935e92eb592c563d135",
+    "url": "/react-js-resume/static/js/runtime-main.805f85ed.js"
+  }
+]);
